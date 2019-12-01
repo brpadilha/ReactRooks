f37688f99a6a495fa468cbc0e09cbd7c0995d0f2
Bruno Padilha <brpadilha.dev@gmail.com>
2019-12-01 18:42:07 -0200

    Hoock useState

--- src/App.js
+++ src/App.js
@@ -1,8 +1,25 @@
-import React from 'react';
-import Routes from './routes';
+import React, { useState } from 'react';
 
 function App() {
-  return <Routes />;
+  const [tech, setTech] = useState(['ReactJS', 'React Native']);
+  const [newTech, setNewTech] = useState('');
+
+  function handleAdd() {
+    setTech([...tech, newTech]);
+  }
+  return (
+    <>
+      <ul>
+        {tech.map(t => (
+          <li key={t}>{t}</li>
+        ))}
+      </ul>
+      <input value={newTech} onChange={e => setNewTech(e.target.value)}></input>
+      <button type="button" onClick={handleAdd}>
+        Adicionar
+      </button>
+    </>
+  );
 }
 
 export default App;
